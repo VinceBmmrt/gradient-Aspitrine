d705ffc233bbe62cd6a0992e1e573b41d29da711
Aspitrine <loquente62@gmail.com>
2023-11-08 11:49:08 +0100

    Ajout de React Redux + useSelector

--- src/components/NbColors/NbColors.tsx
+++ src/components/NbColors/NbColors.tsx
@@ -1,5 +1,20 @@
+import { useSelector } from 'react-redux';
+
 function NbColors() {
-  return <div className="nbColors">XXX couleur(s) générée(s)</div>;
+  // useSelector va me permettre de récupérer des données depuis mon store
+  // useSelector va prendre en paramètre une fonction de callback
+  // Cette fonction me permet de récupérer le state de mon store redux
+  // Je dois retourner depuis les données stocker dans `state` la donnée qui m'intéresse
+  // le useSelector est l'équivalent d'un getState et d'un subscribe
+  // Lorsque les données du store vont être modifier, la fonction passé dans le useSelector va s'exécuter
+  // Si la donnée retourner par la fonction à changer, le composant va se mettre à jour
+  const nbColors = useSelector((state) => {
+    // Cette fonction sera exécuter à chaque fois que le state va changer
+    // Mais le composant sera re-rendu uniquement si la donnée retourné par cette fonction change
+    return state.color.nbColors;
+  });
+
+  return <div className="nbColors">{nbColors} couleur(s) générée(s)</div>;
 }
 
 export default NbColors;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
-
+import store from './store';
 import './styles/index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLDivElement
 );
 
-root.render(<App />);
+// Pour fournir les données de mon store à mon application,
+// Je dois englober mon application dans un composant `Provider`
+// Et fournir au provider mon store redux
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
