01d61f4c77b0b064a4e9d611591b82a015fdab98
Aspitrine <loquente62@gmail.com>
2023-11-08 14:01:55 +0100

    Utilisation du useAppDispatch pour emettre des intentions dans le composant DirectionButtons

--- src/components/DirectionButtons/DirectionButtons.tsx
+++ src/components/DirectionButtons/DirectionButtons.tsx
@@ -1,22 +1,58 @@
+import { TDirection } from '../../@types';
+import { useAppDispatch } from '../../hooks/redux';
+import { changeDirection } from '../../store/reducers/color';
+
 function DirectionButtons() {
+  const dispatch = useAppDispatch();
+
+  const handleClickDirection = (direction: TDirection) => {
+    // monAction === { payload: direction, type: "color/changeDirection" }
+    const monAction = changeDirection(direction);
+    dispatch(monAction);
+  };
+
   return (
     <div className="buttons group">
-      <button type="button" className="button">
+      <button
+        type="button"
+        className="button"
+        onClick={() => handleClickDirection('45deg')}
+      >
         45deg
       </button>
-      <button type="button" className="button">
+      <button
+        type="button"
+        className="button"
+        onClick={() => handleClickDirection('90deg')}
+      >
         90deg
       </button>
-      <button type="button" className="button">
+      <button
+        type="button"
+        className="button"
+        onClick={() => handleClickDirection('135deg')}
+      >
         135deg
       </button>
-      <button type="button" className="button">
+      <button
+        type="button"
+        className="button"
+        onClick={() => handleClickDirection('225deg')}
+      >
         225deg
       </button>
-      <button type="button" className="button">
+      <button
+        type="button"
+        className="button"
+        onClick={() => handleClickDirection('270deg')}
+      >
         270deg
       </button>
-      <button type="button" className="button">
+      <button
+        type="button"
+        className="button"
+        onClick={() => handleClickDirection('315deg')}
+      >
         315deg
       </button>
     </div>
