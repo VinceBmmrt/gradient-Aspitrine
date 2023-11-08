69955044aa5fd0f33305fa63c5a70291b862be02
Aspitrine <loquente62@gmail.com>
2023-11-08 12:04:01 +0100

    Ajout des hooks pre typer de redux

--- src/components/NbColors/NbColors.tsx
+++ src/components/NbColors/NbColors.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useAppSelector } from '../../hooks/redux';
 
 function NbColors() {
   // useSelector va me permettre de récupérer des données depuis mon store
@@ -8,7 +8,7 @@ function NbColors() {
   // le useSelector est l'équivalent d'un getState et d'un subscribe
   // Lorsque les données du store vont être modifier, la fonction passé dans le useSelector va s'exécuter
   // Si la donnée retourner par la fonction à changer, le composant va se mettre à jour
-  const nbColors = useSelector((state) => {
+  const nbColors = useAppSelector((state) => {
     // Cette fonction sera exécuter à chaque fois que le state va changer
     // Mais le composant sera re-rendu uniquement si la donnée retourné par cette fonction change
     return state.color.nbColors;
