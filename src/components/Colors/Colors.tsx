befb50f2fce4f001070a87aed2eeb8f05733cb57
Aspitrine <loquente62@gmail.com>
2023-11-08 13:33:31 +0100

    Récupération des données depuis redux pour les composants Gradient / Colors

--- src/components/Colors/Colors.tsx
+++ src/components/Colors/Colors.tsx
@@ -1,8 +1,14 @@
+import { useAppSelector } from '../../hooks/redux';
+
 function Colors() {
+  // On évite absolument de retourner un objet depuis le useSelector car on s'abonne à la modification de la donnée retourné par le useSelector
+  // On veut éviter les re-rendu inutile, on essaye d'être le plus précis possible lors de la récupération des données
+  const firstColor = useAppSelector((state) => state.color.firstColor);
+  const lastColor = useAppSelector((state) => state.color.lastColor);
   return (
     <div className="colors">
-      <span style={{ color: '#F0F' }}>#F0F</span>
-      <span style={{ color: '#0F0' }}>#0F0</span>
+      <span style={{ color: firstColor }}>{firstColor}</span>-
+      <span style={{ color: lastColor }}>{lastColor}</span>
     </div>
   );
 }
--- src/components/Grandient/Gradient.tsx
+++ src/components/Grandient/Gradient.tsx
@@ -1,8 +1,15 @@
+import { useAppSelector } from '../../hooks/redux';
+
 function Gradient() {
+  const firstColor = useAppSelector((state) => state.color.firstColor);
+  const lastColor = useAppSelector((state) => state.color.lastColor);
+  const direction = useAppSelector((state) => state.color.direction);
   return (
     <div
       className="gradient"
-      style={{ background: 'linear-gradient(90deg, #F0F, #0F0)' }}
+      style={{
+        background: `linear-gradient(${direction}, ${firstColor}, ${lastColor})`,
+      }}
     />
   );
 }
