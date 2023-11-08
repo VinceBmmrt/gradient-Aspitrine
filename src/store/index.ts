62677bc83f24dda377b1076faabc07e881802907
Aspitrine <loquente62@gmail.com>
2023-11-08 14:05:30 +0100

    fix export par défaut le reducer à la place du slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import colorSlice from './reducers/color';
+import colorReducer from './reducers/color';
 
 const store = configureStore({
   // A la création du store
   // Redux va exécuter chaque reducer avec l'action "@@INIT"
   reducer: {
-    color: colorSlice.reducer,
+    color: colorReducer,
   },
 });
 
--- src/store/reducers/color.ts
+++ src/store/reducers/color.ts
@@ -52,4 +52,4 @@ export const {
   changeDirection,
 } = colorSlice.actions;
 
-export default colorSlice;
+export default colorSlice.reducer;
